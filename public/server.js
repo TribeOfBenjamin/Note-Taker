60c47cd2dc62fa8530763b7c0e53e6195249abe1
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-06-16 22:40:16 -0500

    cleaned up comments

--- public/server.js
+++ public/server.js
@@ -17,26 +17,26 @@ const notesData = require("../db/db.json");
 
 // Routes
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
-  });
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/api/notes", function(req, res) {
-//    fs.readFile(notesData, "utf8", function(err, data) {
-//        if (err) throw err;
-//    })
+app.get("/api/notes", function (req, res) {
+    //    fs.readFile(notesData, "utf8", function(err, data) {
+    //        if (err) throw err;
+    //    })
     return res.json(notesData);
 })
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // app.post("/api/notes", function(req, res) {
 
 // })
 
 // Server Begins Listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
