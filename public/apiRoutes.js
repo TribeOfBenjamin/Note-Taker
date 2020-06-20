e44c4288f64747402a8b9f30dbf1bce9b4f9b738
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-06-20 14:54:01 -0500

    fixed render problem; added unique id to each note

--- public/apiRoutes.js
+++ public/apiRoutes.js
@@ -1,34 +1,48 @@
 const fs = require("fs");
+const path = require("path");
 
 const notesData = require("../db/db.json");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/notes", function(req, res) {
-        // Commented out below used to be here; fs package not needed?
+    app.get("/api/notes", function (req, res) {
         res.json(notesData);
     });
 
-    app.post("/api/notes", function(req, res) {
-        notesData.push(req.body);
+    app.post("/api/notes", function (req, res) {
+
+        let newNote = req.body;
+        let noteId = Math.random();
+        newNote.id = noteId;
+
+        notesData.push(newNote);
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData),
+            err => {
+                if (err) throw err;
+            }
+        );
 
         // Below is intended to clear note upon save BUT doesn't
         // $(".note-title").val("");
         // $(".note-textarea").val("");
 
-        return notesData;
-        // Is this ^^^ what is meant by returning the new note to the client?
-        // res.json(true); <--What is this doing in Hot Restaurant?
+        res.json(newNote);
     });
 
     // MY ATTEMPT AT DELETE
-    app.delete("/api/notes/:id", function(req, res) {
+    app.delete("/api/notes/:id", function (req, res) {
         let deletedNote = req.params.id;
-        console.log(deletedNote);
-    })
-};
 
-// fs.readFile(notesData, "utf8", function (err, data) {
-//     if (err) throw err;
-// })
-// return
+        function deleteFunction(note) {
+
+            for (let i = 0; i < notesData.length; i++) {
+                if (notesData[i].id === deletedNote) {
+                    notesData[i].splice(i, 1);
+                }
+            }
+        }
+        deleteFunction();
+        res.json(notesData);
+    });
+};
